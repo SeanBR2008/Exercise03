b1f3dca1597e45ff55eca871ffe8c4877076d77e
SeanBR2008 <2587397@cstjean.qc.ca>
2025-08-26 10:42:01 -0400

    last update?

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int WATTS_PAR_HEURE = 150;
+const double COUTS_POUR_100WATTS_PAR_HEURE = 0.15;
+const int WATTS_100 = 100;
+
+double Watts;
+double Heure;
+
+Console.WriteLine("inscrivez le nombre heure.");
+Console.ReadLine;
+Console.clear();
+
+couts = nombreHeure * WATTS_PAR_HEURE / WATTS_100 * COUTS_POUR_100WATTS_PAR_HEURE;
+
+Console.WriteLine("Cout pour le nombre d'heure donner:" + couts);
